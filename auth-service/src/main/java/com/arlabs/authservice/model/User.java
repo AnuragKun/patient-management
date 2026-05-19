9cc9aea6f8eb795ec995d21ea9f38b3f1e91200a
AnuragKun <anuragrana578@gmail.com>
2026-05-19 22:11:53 +0530

    Jwt , api gateway, auth service addition

---
+++ auth-service/src/main/java/com/arlabs/authservice/model/User.java
@@ -0,0 +1,28 @@
+package com.arlabs.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
---
+++ auth-service/src/main/java/com/arlabs/authservice/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.arlabs.authservice.service;
+
+import com.arlabs.authservice.dto.LoginRequestDTO;
+import com.arlabs.authservice.model.User;
+import com.arlabs.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+
+        return userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+    }
+}
